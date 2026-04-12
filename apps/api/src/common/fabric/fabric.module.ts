edbd2d74935544aee03ac55ca299f59acdb0a2ca
Ahmed Hammad <38661241+ahmedshammad@users.noreply.github.com>
2026-04-12 17:55:30 +0200

    Delete apps/api directory

--- apps/api/src/common/fabric/fabric.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { FabricService } from './fabric.service';
-
-@Global()
-@Module({
-  providers: [FabricService],
-  exports: [FabricService],
-})
-export class FabricModule {}
